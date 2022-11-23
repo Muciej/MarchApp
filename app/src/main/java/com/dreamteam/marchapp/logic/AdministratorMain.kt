c87fdf8df83c046c066dc0fa63f54ba934cec209
S2n0a0k0e <88397167+S2n0a0k0e@users.noreply.github.com>
2022-11-23 21:51:29 +0100

    Created logic to administrator basic screens

---
+++ app/src/main/java/com/dreamteam/marchapp/logic/AdministratorMain.kt
@@ -0,0 +1,11 @@
+package com.dreamteam.marchapp.logic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdministratorMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_administrator_main)
+    }
+}
---
+++ app/src/main/java/com/dreamteam/marchapp/logic/CreateVolunteerActivity.kt
@@ -0,0 +1,11 @@
+package com.dreamteam.marchapp.logic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateVolunteerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_volunteer)
+    }
+}
