4c8250c59477a4485710005afd0e562d0e76809e
holubek01 <261718@student.pwr.edu.pl>
2023-05-18 09:51:00 +0200

    improve ShowAndEditParticipant

--- app/src/main/java/com/dreamteam/marchapp/logic/shared/ShowAndEditParticipant.kt
+++ app/src/main/java/com/dreamteam/marchapp/logic/shared/ShowAndEditParticipant.kt
@@ -1,10 +1,10 @@
 package com.dreamteam.marchapp.logic.shared
 
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.text.Editable
 import android.text.SpannableString
@@ -20,7 +20,6 @@ import androidx.lifecycle.ViewModelProvider
 import com.dreamteam.marchapp.R
 import com.dreamteam.marchapp.R.id.*
 import com.dreamteam.marchapp.database.DataViewModel
-import com.dreamteam.marchapp.database.JDBCConnector
 import com.dreamteam.marchapp.database.dataclasses.Participant
 import com.dreamteam.marchapp.logic.admin.AdministratorMain
 import com.dreamteam.marchapp.logic.organiser.OrganisatorMain
@@ -38,60 +37,82 @@ import kotlinx.android.synthetic.main.dialog_edit_user.view.*
 import kotlinx.android.synthetic.main.dialog_zoom_data.view.backb
 import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.reflect.KProperty1
+import com.dreamteam.marchapp.databinding.ActivityOrganiserShowVolunteersBinding
 
 class ShowAndEditParticipant : AppCompatActivity(), TableDataClickListener<Array<String>>{
     private var lastClickedRow = -1
     var data : MutableList<Array<String>> = mutableListOf()
-    lateinit var adapterData: SimpleTableDataAdapter
     lateinit var tableView : TableView<Array<String>>
-    private lateinit var editModeCheckbox : CheckBox
-    private lateinit var backBtn : Button
     private lateinit var adapter : ArrayAdapter<String>
-    lateinit var customSpinner : TextView
     private lateinit var participantList :MutableList<Participant>
-    var participantNames = Vector<String>()
+    private var participantNames = Vector<String>()
     private lateinit var dataViewModel: DataViewModel
+    private lateinit var binding: ActivityOrganiserShowVolunteersBinding
+    private lateinit var dataAdapter : SimpleTableDataAdapter
+
+    //lista uczestników do pokazania w tabeli
+    private lateinit var participantsToShow : MutableList<Array<String>>
+    var accessLevel = ""
 
     private fun participantUpdated(newParticipants: ArrayList<Participant>) {
         participantList = newParticipants
         participantNames.clear()
         participantNames.add("wszyscy")
-        for(participant in newParticipants){
-            participantNames.add("${participant.name} ${participant.surname}")
-        }
+        participantNames.addAll(newParticipants.map { "${it.name} ${it.surname}" })
         participantNames.add("nie wybrano")
 
-        adapter = ArrayAdapter(this@ShowAndEditParticipant, android.R.layout.simple_list_item_1, participantNames)
+        init_spinner_and_table()
+    }
 
-        adapterData = SimpleTableDataAdapter(this@ShowAndEditParticipant, initData())
-        adapterData.setTextSize(12)
-        adapterData.setTextColor(Color.BLACK)
-        tableView.dataAdapter = adapterData
+    private fun init_spinner_and_table() {
+        adapter = ArrayAdapter(this@ShowAndEditParticipant, android.R.layout.simple_list_item_1, participantNames)
+        participantsToShow = initData()
+
+        dataAdapter.data.clear()
+        dataAdapter.addAll(participantsToShow)
+        dataAdapter.notifyDataSetChanged()
     }
 
 
-
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_organiser_show_volunteers)
+        binding = ActivityOrganiserShowVolunteersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         dataViewModel = ViewModelProvider(this)[DataViewModel::class.java]
-        dataViewModel.allParticipants.observe(this, androidx.lifecycle.Observer {
-                newParticipants -> participantUpdated(newParticipants)
-        })
+        dataViewModel.allParticipants.observe(this) { newParticipants ->
+            participantUpdated(newParticipants)
+        }
 
-        customSpinner = findViewById(textView)
-        backBtn = findViewById(btnBack)
-        editModeCheckbox = findViewById(editCheckbox)
-        val title = findViewById<TextView>(ChooseVolonteersTextView)
-        val hint = findViewById<TextView>(textView)
-
-        hint.text = "Wybierz uczestnika"
-        title.text = "Wybierz uczestnika"
-
+        binding.customSpinner.text = "Wybierz uczestnika"
+        binding.ChooseVolonteersTextView.hint= "Wybierz uczestnika"
 
         //inicjalizacja tabeli
+        setTable()
+
+        accessLevel = intent.getStringExtra("accessLevel")!!
+        if (accessLevel == "Organiser" || accessLevel == "Volunteer")
+        {
+            binding.editCheckbox.isClickable=false
+            binding.editCheckbox.visibility=View.INVISIBLE
+        }
+
+        binding.btnBack.setOnClickListener{ goBack() }
+        binding.customSpinner.setOnClickListener{ show_dialog() }
+    }
+
+    private fun goBack() {
+        lateinit var intent : Intent
+        when (accessLevel) {
+            "Organiser" -> intent = Intent(this, OrganisatorMain::class.java)
+            "Admin" -> intent = Intent(this, AdministratorMain::class.java)
+            "Volunteer" -> intent = Intent(this, VolunteerMain::class.java)
+        }
+        startActivity(intent)
+    }
+
+    private fun setTable() {
         tableView = findViewById<View>(volunteersTable) as TableView<Array<String>>
         tableView.addDataClickListener(this)
         tableView.setHeaderBackgroundColor(Color.rgb(		98, 0, 238))
@@ -110,7 +131,6 @@ class ShowAndEditParticipant : AppCompatActivity(), TableDataClickListener<Array
         tableView.headerAdapter = adapterHead
         adapterHead.setTextSize(15)
         adapterHead.setTextColor(Color.WHITE)
-        tableView.dataAdapter = SimpleTableDataAdapter(this, arrayOf())
 
         val columnModel = TableColumnPxWidthModel(5, 200)
         columnModel.setColumnWidth(0, 80)
@@ -121,37 +141,18 @@ class ShowAndEditParticipant : AppCompatActivity(), TableDataClickListener<Array
         tableView.columnModel = columnModel
 
 
+        participantsToShow = arrayListOf()
+        dataAdapter = SimpleTableDataAdapter(this@ShowAndEditParticipant, participantsToShow)
 
-
-        val accessLevel = intent.getStringExtra("accessLevel")
-        if (accessLevel == "Organiser" || accessLevel == "Volunteer")
-        {
-            editModeCheckbox.isClickable=false
-            editModeCheckbox.visibility=View.INVISIBLE
-        }
-
-
-        backBtn.setOnClickListener{
-            lateinit var intent : Intent
-            when (accessLevel) {
-                "Organiser" -> intent = Intent(this, OrganisatorMain::class.java)
-                "Admin" -> intent = Intent(this, AdministratorMain::class.java)
-                "Volunteer" -> intent = Intent(this, VolunteerMain::class.java)
-            }
-            startActivity(intent)
-        }
-
-
-        customSpinner.setOnClickListener{ show_dialog() }
+        dataAdapter.setTextSize(12)
+        dataAdapter.setTextColor(Color.BLACK)
+        tableView.dataAdapter = dataAdapter
     }
-
 
 
     private fun show_dialog() {
         val dialog = Dialog(this@ShowAndEditParticipant)
         dialog.setContentView(R.layout.dialog_searchable_spinner)
-        dialog.window?.setLayout(800,800)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.show()
 
         val edittext = dialog.findViewById<EditText>(edit_text)
@@ -169,15 +170,17 @@ class ShowAndEditParticipant : AppCompatActivity(), TableDataClickListener<Array
 
 
         listview.onItemClickListener =
-            AdapterView.OnItemClickListener { _, _, p2, _ ->
-                customSpinner.text = adapter.getItem(p2)
+            AdapterView.OnItemClickListener { _, _, clicked, _ ->
+                binding.customSpinner.text = adapter.getItem(clicked)
 
-                adapterData = if (listview.getItemAtPosition(p2).toString().lowercase() == "wszyscy") SimpleTableDataAdapter(this@ShowAndEditParticipant, initData())
-                else if (listview.getItemAtPosition(p2).toString().lowercase()!="nie wybrano") SimpleTableDataAdapter(this@ShowAndEditParticipant, selectData(listview.getItemAtPosition(p2).toString()))
-                else SimpleTableDataAdapter(this@ShowAndEditParticipant, arrayOf())
-                adapterData.setTextSize(12)
-                adapterData.setTextColor(Color.BLACK)
-                tableView.dataAdapter = adapterData
+                if (listview.getItemAtPosition(clicked).toString().lowercase() == "wszyscy") participantsToShow = initData()
+                else if (listview.getItemAtPosition(clicked).toString().lowercase()!="nie wybrano") participantsToShow = selectData(listview.getItemAtPosition(clicked).toString())
+                else participantsToShow.clear()
+
+                dataAdapter.data.clear()
+                dataAdapter.addAll(participantsToShow)
+                dataAdapter.notifyDataSetChanged()
+
                 dialog.dismiss()
             }
 
@@ -185,8 +188,8 @@ class ShowAndEditParticipant : AppCompatActivity(), TableDataClickListener<Array
 
     private fun selectData(name: String):MutableList<Array<String>>
     {
-        val dataa = name.split(" ")
-        val participant:Participant = participantList!!.filter { it.name == dataa[0] && it.surname == dataa[1] }.first()
+        val tmp = name.split(" ")
+        val participant:Participant = participantList!!.filter { it.name == tmp[0] && it.surname == tmp[1] }.first()
 
         data.clear()
         data.add(arrayOf(
@@ -224,8 +227,8 @@ class ShowAndEditParticipant : AppCompatActivity(), TableDataClickListener<Array
 
          */
 
-        if (field == "nr_startowy") return participantList.any { participant -> participant.startNumber == obj.toInt()}
-        else if (field == "pseudonim") return participantList.any { participant -> participant.nickname == obj}
+        if (field == "nr_startowy") return participantList.any { participant -> participant.startNumber == obj.toInt() && participant.accId != id}
+        else if (field == "pseudonim") return participantList.any { participant -> participant.nickname == obj && participant.accId != id}
         return false
     }
 
@@ -233,20 +236,15 @@ class ShowAndEditParticipant : AppCompatActivity(), TableDataClickListener<Array
 
         lastClickedRow = rowIndex
 
-        if (!editModeCheckbox.isChecked)
+        if (!binding.editCheckbox.isChecked)
         {
             class MyCustomDialog: DialogFragment() {
-
-
                 override fun onCreateView(
                     inflater: LayoutInflater,
                     container: ViewGroup?,
                     savedInstanceState: Bundle?
                 ): View {
 
-                    dialog!!.window?.setBackgroundDrawableResource(R.drawable.round_corners)
-                    dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-                    dialog?.window?.setLayout(1000,900)
                     val rootView : View = inflater.inflate(R.layout.dialog_zoom_data_user, container, false)
                     rootView.backb.setOnClickListener { dismiss() }
 
@@ -302,90 +300,37 @@ class ShowAndEditParticipant : AppCompatActivity(), TableDataClickListener<Array
                     container: ViewGroup?,
                     savedInstanceState: Bundle?
                 ): View {
-
-
-                    dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
                     val rootView : View = inflater.inflate(R.layout.dialog_edit_user, container, false)
                     rootView.backb.setOnClickListener { dismiss() }
-
-                    rootView.editButton.setOnClickListener {
-                        //chcemy do bazy danych i do tabeli wklepać wartości z edycji
-
-                        //nowe wartości
-                        val editedName = edit_name.text.toString()
-                        val editedLastName = lastname_edit.text.toString()
-                        val editedStartNr = start_no_edit.text.toString()
-                        val editedPsudonim = pseudonim_edit.text.toString()
-
-                        val rowToEdit: Int
-                        rowToEdit = clickedData!![0].toInt()
-                        val qrCode = participantList.find { participant ->
-                            participant.accId == clickedData[0].toInt()
-                        }?.qrCodeData
-
-
-                        if (editedName.isBlank() || editedLastName.isBlank() ||
-                            editedPsudonim.isBlank() || editedStartNr.isBlank()
-                        ) {
-                            Toast.makeText(this@ShowAndEditParticipant, "Żadne z pól nie może być puste", Toast.LENGTH_SHORT).show()
-                        } else {
-                            var isCorrect = true
-
-
-                            if (!NameValidator.validate(editedName)) {
-                                Toast.makeText(
-                                    this@ShowAndEditParticipant,
-                                    "Nieprawidłowy format imienia!",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                isCorrect = false
-                            } else if (!LastNameValidator.validate(editedLastName)) {
-                                Toast.makeText(
-                                    this@ShowAndEditParticipant,
-                                    "Nieprawidłowy format nazwiska!",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                isCorrect = false
-                            }
-
-                            else if (checkIfPresentInDB(editedStartNr, "nr_startowy", rowToEdit ))
-                            {
-                                Toast.makeText(
-                                    this@ShowAndEditParticipant,
-                                    "Osoba o podanym nr startowym już istnieje w bazie!",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                isCorrect = false
-                            }
-                            else if (checkIfPresentInDB(editedPsudonim, "pseudonim", rowToEdit))
-                            {
-                                Toast.makeText(
-                                    this@ShowAndEditParticipant,
-                                    "Osoba o podanym pseudonimie już istnieje w bazie!",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                isCorrect = false
-                            }
-
-
-                            //Po aktualizacji wracam do ekranu głównego administratora.
-
-                            if (isCorrect) {
-
-                                val editedParticipant = Participant(rowToEdit, editedStartNr.toInt(), editedName, editedLastName, editedPsudonim, qrCode!!)
-
-                                dataViewModel.updateParticipant(editedParticipant)
-                                customSpinner.text = ""
-                                dismiss()
-                            }
-                        }
-                    }
-
 
                     val name = rootView.findViewById<EditText>(R.id.edit_name)
                     val lastname = rootView.findViewById<EditText>(R.id.lastname_edit)
                     val start_nr = rootView.findViewById<EditText>(R.id.start_no_edit)
                     val pseudo = rootView.findViewById<EditText>(R.id.pseudonim_edit)
+
+                    rootView.editButton.setOnClickListener {
+
+                        //nowe wartości
+                        val editedName = edit_name.text.toString()
+                        val editedLastName = lastname_edit.text.toString()
+                        val editedStartNr = start_no_edit.text.toString()
+                        val editedPseudonim = pseudonim_edit.text.toString()
+
+                        val editedParticipantId: Int = clickedData!![0].toInt()
+
+                        val qrCode = participantList.find { participant ->
+                            participant.accId == clickedData[0].toInt()
+                        }?.qrCodeData
+
+                        val isCorrect = validateEditedData(editedName, editedLastName, editedStartNr, editedPseudonim, editedParticipantId)
+
+                        if (isCorrect) {
+                            val editedParticipant = Participant(editedParticipantId, editedStartNr.toInt(), editedName, editedLastName, editedPseudonim, qrCode!!)
+                            dataViewModel.updateParticipant(editedParticipant)
+                            binding.customSpinner.text = ""
+                            dismiss()
+                        }
+                    }
 
                     name.setText(clickedData?.get(2))
                     lastname.setText(clickedData?.get(3))
@@ -395,9 +340,50 @@ class ShowAndEditParticipant : AppCompatActivity(), TableDataClickListener<Array
                     return rootView
                 }
 
+                private fun validateEditedData(
+                    editedName: String,
+                    editedLastName: String,
+                    editedStartNr: String,
+                    editedPseudonim: String,
+                    editedParticipantId: Int
+                ): Boolean {
+                    if (editedName.isBlank() || editedLastName.isBlank() ||
+                        editedPseudonim.isBlank() || editedStartNr.isBlank()
+                    ) {
+                        Toast.makeText(this@ShowAndEditParticipant, "Żadne z pól nie może być puste", Toast.LENGTH_SHORT).show()
+                        return false
+                    }
+
+                    if (!NameValidator.validate(editedName)) {
+                        showInfo("Nieprawidłowy format imienia!")
+                        return false
+                    }
+
+                    if (!LastNameValidator.validate(editedLastName)) {
+                        showInfo("Nieprawidłowy format nazwiska!")
+                        return false
+                    }
+
+                    if (checkIfPresentInDB(editedStartNr, "nr_startowy", editedParticipantId )) {
+                        showInfo("Osoba o podanym nr startowym już istnieje w bazie!")
+                        return false
+                    }
+
+                    if (checkIfPresentInDB(editedPseudonim, "pseudonim", editedParticipantId)) {
+                        showInfo("Osoba o podanym pseudonimie już istnieje w bazie!")
+                        return false
+                    }
+
+                    return true
+                }
+
+
+                private fun showInfo(mess: String) {
+                    Toast.makeText(this@ShowAndEditParticipant, mess, Toast.LENGTH_SHORT).show()
+                }
+
                 override fun onStart() {
                     super.onStart()
-                    dialog!!.window?.setBackgroundDrawableResource(R.drawable.round_corners)
                     val defaultDisplay = DisplayManagerCompat.getInstance(requireContext()).getDisplay(
                         Display.DEFAULT_DISPLAY
                     )
@@ -412,8 +398,8 @@ class ShowAndEditParticipant : AppCompatActivity(), TableDataClickListener<Array
             val dialog  = MyCustomEditDialog()
             dialog.show(supportFragmentManager, "CustomEditDialog")
         }
-        }
     }
+}
 
 
 
