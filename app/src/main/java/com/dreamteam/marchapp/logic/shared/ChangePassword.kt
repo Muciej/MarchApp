747ee3aefe3aec3d38e57e274579fbd3256a8d0e
mdyblik <mdyblik01@gmail.com>
2023-05-22 18:59:34 +0200

    Use view model in Change Password

--- app/src/main/java/com/dreamteam/marchapp/logic/shared/ChangePassword.kt
+++ app/src/main/java/com/dreamteam/marchapp/logic/shared/ChangePassword.kt
@@ -6,51 +6,31 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.dreamteam.marchapp.R
-import com.dreamteam.marchapp.database.DBConnector
-import com.dreamteam.marchapp.database.JDBCConnector
+import com.dreamteam.marchapp.database.DataViewModel
+import com.dreamteam.marchapp.database.dataclasses.Roles
 import com.dreamteam.marchapp.logic.admin.AdministratorMain
 import com.dreamteam.marchapp.logic.config.PasswordEncoder
 import com.dreamteam.marchapp.logic.organiser.OrganisatorMain
 import com.dreamteam.marchapp.logic.participant.RegisterParticipant
-import com.dreamteam.marchapp.logic.validation.EmailValidator
 import com.dreamteam.marchapp.logic.validation.PasswordValidator
-import com.dreamteam.marchapp.logic.validation.PhoneValidator
 import com.dreamteam.marchapp.logic.volunteer.VolunteerMain
 
 
 class ChangePassword : AppCompatActivity() {
-    var connector: DBConnector = JDBCConnector
-
-    fun changePassword() : Boolean {
-        val newPassword = findViewById<TextView>(R.id.newPassword)
-
-        connector.startConnection()
-        var CurrentUserID = connector.getCurrentUserID()
-        val hashedPass = PasswordEncoder.hash(newPassword.text.toString())
-
-        try {
-            connector.prepareQuery("UPDATE konta SET hasło = '${hashedPass}' WHERE id_konta LIKE '${CurrentUserID}';")
-            connector.executeQuery()
-        } catch (e: Exception) {
-            Toast.makeText(this, "Problem ze polaczeniem\n z baza danych", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        connector.closeQuery()
-        connector.closeConnection()
-        return true
-    }
+    private lateinit var dataViewModel: DataViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password)
+        dataViewModel = ViewModelProvider(this)[DataViewModel::class.java]
 
         val oldPassword = findViewById<TextView>(R.id.oldPassword)
         val newPassword = findViewById<TextView>(R.id.newPassword)
         val repeatPassword = findViewById<TextView>(R.id.repeatPassword)
         val changePassButt = findViewById<Button>(R.id.changePassButt)
         val backBtn = findViewById<Button>(R.id.btnBack)
-
 
         backBtn.setOnClickListener {
             previousPage()
@@ -79,8 +59,10 @@ class ChangePassword : AppCompatActivity() {
                     isCorrect = false
                 }
 
+                changePassword()
+
                 //Po aktualizacji wracam do ekranu głównego administratora.
-                if (isCorrect && changePassword()) {
+                if (isCorrect) {
                     Toast.makeText(
                         this,
                         "Zmiana hasła przebiegła poprawnie!",
@@ -93,34 +75,32 @@ class ChangePassword : AppCompatActivity() {
         }
     }
 
-    fun previousPage(){
+    private fun changePassword() {
+        val newPassword = findViewById<TextView>(R.id.newPassword)
 
-        connector.startConnection()
-        var CurrentUserID = connector.getCurrentUserID()
-        connector.prepareQuery("select rola_id from konta where id_konta like '${CurrentUserID}';")
-        connector.executeQuery()
-        var rola = Integer.parseInt(connector.getCol(1)[0])
-        connector.closeQuery()
-        connector.closeConnection()
+        val hashedPass = PasswordEncoder.hash(newPassword.text.toString())
+        dataViewModel.changeUserPassword(dataViewModel.loggedAcount.value!!.id, hashedPass)
+    }
 
-        when (rola) {
-            1 -> {
+    private fun previousPage() {
+        when (dataViewModel.loggedAcount.value!!.role) {
+            Roles.ORGANISER -> {
                 val intent = Intent(this, OrganisatorMain::class.java)
                 startActivity(intent)
             }
-            2 -> {
+            Roles.VOLOUNTEER -> {
                 val intent = Intent(this, VolunteerMain::class.java)
                 startActivity(intent)
             }
-            3 -> {
-                // TODO tu powinno odnosić do widoku uczestnika, ale jak na razie on nie istnieje xD
+            Roles.PARTICIPANT -> {
                 val intent = Intent(this, RegisterParticipant::class.java)
                 startActivity(intent)
             }
-            4 -> {
+            Roles.ADMIN -> {
                 val intent = Intent(this, AdministratorMain::class.java)
                 startActivity(intent)
-            } else -> {
+            }
+            else -> {
                 Toast.makeText(
                     this,
                     "Nie posiadasz przypisanej roli",
